a116b90aab61aae7b2cfbb3c6e5aa816a6d30c71
Petr Hlozek <petr@petrhlozek.cz>
2026-09-04 15:49:48 +0200

    feat: support for TOTA (Towers on the Air) references

--- js/app.js
+++ js/app.js
@@ -126,7 +126,7 @@ function handleInput() {
       } else if (item.match(/^[0-5][0-9]{1}$/) && qsotime && itemNumber === 0) {
         qsotime = qsotime.slice(0, -2) + item;
       } else if (
-        item.match(/^([A-Z0-9]{1,4}FF-\d{4}|[A-Z0-9]{1,4}\/[A-Z]{2}-\d{3})$/i)
+        item.match(/^([A-Z0-9]{1,4}FF-\d{4}|[A-Z0-9]{1,4}\/[A-Z]{2}-\d{3}|[A-Z0-9]{2}R-\d{4})$/i)
       ) {
         sotaWff = item.toUpperCase();
       } else if (
@@ -486,12 +486,18 @@ Internet: https://sfle.ok2cqr.com
     } else if (isWWFF(mySotaWwff)) {
       qso = qso + getAdifTag("MY_SIG", "WWFF");
       qso = qso + getAdifTag("MY_SIG_INFO", mySotaWwff);
+    } else if (isTOTA(mySotaWwff)) {
+      qso = qso + getAdifTag("MY_SIG", "TOTA");
+      qso = qso + getAdifTag("MY_SIG_INFO", mySotaWwff);
     }
 
     if (isSOTA(item[8])) {
       qso = qso + getAdifTag("SOTA_REF", item[8]);
     } else if (isWWFF(item[8])) {
       qso = qso + getAdifTag("SIG", "WWFF");
+      qso = qso + getAdifTag("SIG_INFO", item[8]);
+    } else if (isTOTA(item[8])) {
+      qso = qso + getAdifTag("SIG", "TOTA");
       qso = qso + getAdifTag("SIG_INFO", item[8]);
     }
     
@@ -573,6 +579,14 @@ function isSOTA(value) {
 
 function isWWFF(value) {
   if (value.match(/^[A-Z0-9]{1,4}FF-\d{4}$/i)) {
+    return true;
+  }
+
+  return false;
+}
+
+function isTOTA(value) {
+  if (value.match(/^[A-Z0-9]{2}R-\d{4}$/i)) {
     return true;
   }
 
